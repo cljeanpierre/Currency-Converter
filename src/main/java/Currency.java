05c9ea5d8f4a82259b1ca8fdf266afbd9ac268f9
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2023-08-12 23:45:59 -0400

    added constructor for Currency enum

--- src/main/java/Currency.java
+++ src/main/java/Currency.java
@@ -1,2 +1,17 @@
 public enum Currency {
+        USD(1.0),
+        EUR(0.85),
+        GBP(0.76),
+        INR(75.0);
+
+        private double exchangeRateToUSD;
+
+        Currency(double exchangeRateToUSD) {
+            this.exchangeRateToUSD = exchangeRateToUSD;
+        }
+
+        public double getExchangeRateToUSD() {
+            return exchangeRateToUSD;
+        }
+
 }
